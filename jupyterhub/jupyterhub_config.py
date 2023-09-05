843a9a2f14879bd4bf49f17d877c52dae61ebbef
Daniel Perrefort <djperrefort@gmail.com>
2023-09-05 14:19:36 -0400

    Comments out slurm spawner

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -9,8 +9,9 @@
 ##    docker volume rm <name>_jupyterhub_data
 ##
 
-import sys
 import os
+import sys
+
 c = get_config()
 
 ## Generic
@@ -18,11 +19,10 @@ c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 c.Spawner.debug = True
 c.Authenticator.debug = True
-## Authenticator
-import crc_jupyter_auth
-c.JupyterHub.authenticator_class = 'crc_jupyter_auth.RemoteUserAuthenticator'
 
-# These settings are specific to authenticators provided by the crc_jupyter_auth package
+## Authentication
+# These settings are specific to the CRC's proprietary authentication package `crc_jupyter_auth`
+c.JupyterHub.authenticator_class = 'crc_jupyter_auth.RemoteUserAuthenticator'
 c.Authenticator.required_vpn_role = 'SAM-SSLVPNSAMUsers'
 c.Authenticator.missing_user_redirect = 'https://crc.pitt.edu/Access-CRC-Web-Portals'
 c.Authenticator.missing_role_redirect = 'https://crc.pitt.edu/Access-CRC-Web-Portals'
@@ -33,19 +33,19 @@ c.Authenticator.admin_users = {'yak73', 'leb140'}
 
 #####################
 ## Docker spawner
-#c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-#c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
-#c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+# c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+# c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 
 ## user data persistence
-#notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
-#c.DockerSpawner.notebook_dir = notebook_dir
-#c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+# notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+# c.DockerSpawner.notebook_dir = notebook_dir
+# c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
 ## Other stuff
-#c.Spawner.cpu_limit = 1
-#c.Spawner.mem_limit = '1G'
+# c.Spawner.cpu_limit = 1
+# c.Spawner.mem_limit = '1G'
 
 #####################
 
@@ -55,17 +55,16 @@ c.Authenticator.admin_users = {'yak73', 'leb140'}
 c.SlurmSpawner.batch_query_cmd = "squeue -M {cluster} -h -j {job_id} -o '%T %B'"
 c.SlurmSpawner.batch_cancel_cmd = 'scancel -M {cluster} {job_id}'
 
-import batchspawner
-
 # wrapspawner
 c.Spawner.ip = '0.0.0.0'
 c.Spawner.notebook_dir = '~'
 c.Spawner.env_keep = ['PATH', 'LD_LIBRARY_PATH', 'LIBRARY_PATH']
 c.JupyterHub.spawner_class = 'wrapspawner.ProfilesSpawner'
+
 c.ProfilesSpawner.profiles = [
-       	("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip':'0.0.0.0'}),
-        ("Teach Cluster - 1 gpu - 2 cpus, 3 hours", 'teach2c1g3h', 'batchspawner.SlurmSpawner',
-            dict(req_runtime = '3:00:00', req_cluster = 'teach', req_partition = 'gpu', req_options = '-c 2 --mem=24G --gres=gpu:1', req_srun = '')),
+    ("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip': '0.0.0.0'}),
+    # ("Teach Cluster - 1 gpu - 2 cpus, 3 hours", 'teach2c1g3h', 'batchspawner.SlurmSpawner',
+    # dict(req_runtime='3:00:00', req_cluster='teach', req_partition='gpu', req_options='-c 2 --mem=24G --gres=gpu:1', req_srun='')),
 ]
 
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
