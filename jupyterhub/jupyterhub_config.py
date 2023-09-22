e91134dbe7f9693c539f1c94e2ada5e49106d997
Daniel Perrefort <djperrefort@pitt.edu>
2023-09-22 12:37:28 -0400

    Simplify and update recipe for CRC needs (#1)
    * Pulls in changes from prod machine
    
    * Ignores .env files
    
    * Removes traefik service
    
    * Comments out custom commands in jupyterlab config
    
    * Comments out slurm spawner
    
    * Typo fix
    
    * Enables --no-input for pip installs
    
    * Adds pip upgrade command
    
    * Drops env command from jupyterhub dockerfile
    
    * Delete binder dir
    
    * Remove volume mount /usr/bin:/tmp/bin
    
    * Adds temporary mock user
    
    * Disable pip warnings about root installs
    
    * Mount jupyterhub config directly
    
    * Afternoon progress dump
    
    * Fix syntax errors in jupyterhub dockerfile
    
    * Disable ProfilesSpawner.profiles
    
    * Update workdir in jupyterlab dockerfile
    
    * Use podman sock with user permissions
    
    * Specify network name
    
    * Use default jupyterlab image
    
    * Add network definition
    
    * Cache jupyterhub data in volume
    
    * Adds HUB_IP setting

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,60 +1,61 @@
 # JupyterHub configuration
-#
-## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
-##
-##     docker volume rm jupyterhub_jupyterhub_data
-##
-## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
-##
-##    docker volume rm <name>_jupyterhub_data
-##
 
 import os
+import sys
+
+c = get_config()
 
 ## Generic
 c.JupyterHub.admin_access = True
-c.Spawner.default_url = '/lab'
-
-## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
-
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
-
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
-
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
-
-c.Authenticator.admin_users = { 'lucadefe' }
-
-
-## Docker spawner
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
-c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
-# user data persistence
-# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
-c.DockerSpawner.notebook_dir = notebook_dir
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+## Authentication
+# These settings are specific to the CRC's proprietary authentication package `crc_jupyter_auth`
+# c.JupyterHub.authenticator_class = 'crc_jupyter_auth.RemoteUserAuthenticator'
+# c.Authenticator.required_vpn_role = 'SAM-SSLVPNSAMUsers'
+# c.Authenticator.missing_user_redirect = 'https://crc.pitt.edu/Access-CRC-Web-Portals'
+# c.Authenticator.missing_role_redirect = 'https://crc.pitt.edu/Access-CRC-Web-Portals'
+# c.Authenticator.admin_users = {'yak73', 'leb140'}
+c.Authenticator.debug = True
 
-# Other stuff
+## Spawners
+c.Spawner.default_url = '/lab'
+c.Spawner.debug = True
 c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
+c.Spawner.mem_limit = '1G'
 
+# Docker spawner
+# Common convention is to mount mounting user home directories under /home/jovyan/work
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.notebook_dir = '/home/jovyan/work'
+c.DockerSpawner.volumes = {'jupyterhub-user-{username}': '/home/jovyan/work'}
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+
+#c.ProfilesSpawner.profiles = [
+#    ("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip': '0.0.0.0'}),
+#    #("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip': '0.0.0.0'}),
+#    # ("Teach Cluster - 1 gpu - 2 cpus, 3 hours", 'teach2c1g3h', 'batchspawner.SlurmSpawner',
+#    # dict(req_runtime='3:00:00', req_cluster='teach', req_partition='gpu', req_options='-c 2 --mem=24G --gres=gpu:1', req_srun='')),
+#]
 
 ## Services
+c.JupyterHub.load_roles = [
+    {
+        "name": "jupyterhub-idle-culler-role",
+        "scopes": [
+            "list:users",
+            "read:users:activity",
+            "read:servers",
+            "delete:servers",
+        ],
+        "services": ["jupyterhub-idle-culler-service"],
+    }
+]
+
 c.JupyterHub.services = [
     {
-        'name': 'cull_idle',
-        'admin': True,
-        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
-    },
+        "name": "jupyterhub-idle-culler-service",
+        "command": [sys.executable, "-m", "jupyterhub_idle_culler", "--timeout=3600" ],
+    }
 ]
