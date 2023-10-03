61e60cbb3dcbdbab2debb90d793e425c425b56f6
Daniel Perrefort <djperrefort@gmail.com>
2023-10-03 11:55:11 -0400

    Addresses codacy inspections

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,4 +1,4 @@
-# JupyterHub configuration
+"""JupyterHub configuration settings"""
 
 import os
 import sys
@@ -25,19 +25,12 @@ c.Spawner.cpu_limit = 1
 c.Spawner.mem_limit = '1G'
 
 # Docker spawner
-# Common convention is to mount mounting user home directories under /home/jovyan/work
+# Common convention is to mount user home directories under /home/jovyan/work
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.notebook_dir = '/home/jovyan/work'
 c.DockerSpawner.volumes = {'jupyterhub-user-{username}': '/home/jovyan/work'}
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-
-#c.ProfilesSpawner.profiles = [
-#    ("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip': '0.0.0.0'}),
-#    #("Host Process", 'local', 'jupyterhub.spawner.LocalProcessSpawner', {'ip': '0.0.0.0'}),
-#    # ("Teach Cluster - 1 gpu - 2 cpus, 3 hours", 'teach2c1g3h', 'batchspawner.SlurmSpawner',
-#    # dict(req_runtime='3:00:00', req_cluster='teach', req_partition='gpu', req_options='-c 2 --mem=24G --gres=gpu:1', req_srun='')),
-#]
 
 ## Services
 c.JupyterHub.load_roles = [
@@ -56,6 +49,6 @@ c.JupyterHub.load_roles = [
 c.JupyterHub.services = [
     {
         "name": "jupyterhub-idle-culler-service",
-        "command": [sys.executable, "-m", "jupyterhub_idle_culler", "--timeout=3600" ],
+        "command": [sys.executable, "-m", "jupyterhub_idle_culler", "--timeout=3600"],
     }
 ]
